9dec15a121e233cd1feb24d9835ae361250cf4d8
19111441 <ziqiang.liu@haiziwang.com>
2022-03-30 01:15:24 +0800

    code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
-use std::error::Error;
-use std::fs::File;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let f = File::open("hello.txt")?;
+#![allow(unused)]
+fn main() {
+pub struct Guess {
+    value: i32,
+}
 
-    Ok(())
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
 }
