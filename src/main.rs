e9d13c413e2c8272b42cc6e65c6ad738929f5959
19111441 <ziqiang.liu@haiziwang.com>
2022-03-30 01:01:21 +0800

    code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
+use std::fs::File;
 
-#![allow(unused)]
 fn main() {
-use std::fs;
-use std::io;
-
-fn read_username_from_file() -> Result<String, io::Error> {
-    fs::read_to_string("hello.txt")
-}
+    let f = File::open("hello.txt")?;
 }
